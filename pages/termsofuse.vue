ce6cdd0347adb8e57703af02d79666ba54b6cf56
angelaronquillo <142771539+angelaronquillo@users.noreply.github.com>
2024-01-26 12:12:48 +0800

    Update termsofuse.vue

--- pages/termsofuse.vue
+++ pages/termsofuse.vue
@@ -1,5 +1,67 @@
 <template>
-  <div></div>
+  <div class="main">
+    <div class="header">
+      <h2>Terms of Use</h2>
+      <p>Last updated December 16, 2023</p>
+    </div>
+    <div class="content">
+      <h3>I. Section 1</h3>
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+        feugiat vel est a iaculis. Nullam sit amet mi id felis iaculis feugiat.
+        Sed a condimentum orci. Duis sodales libero eget purus sollicitudin
+        condimentum at ullamcorper sem. Donec risus elit, luctus ut cursus eget,
+        rhoncus vel est. Donec sollicitudin justo augue, id dapibus justo
+        imperdiet at. Phasellus et enim sit amet quam facilisis aliquam ac a
+        urna. Aenean tristique urna dui, a semper sem lobortis eu. Aenean auctor
+        euismod velit, vel sollicitudin risus egestas et. Integer sed leo
+        maximus, laoreet magna ac, volutpat sem. Aenean nec sagittis tortor. In
+        facilisis ipsum et imperdiet rhoncus. Nullam lobortis, purus pulvinar
+        elementum aliquet, felis diam venenatis leo, vitae malesuada urna nulla
+        vel risus. Nunc gravida justo lectus.
+      </p>
+      <h3>II. Section 2</h3>
+      <p>
+        Morbi non tellus et mi condimentum malesuada. Pellentesque lacus ante,
+        volutpat eu placerat sed, vulputate in dui. Nullam at ex nec turpis
+        laoreet tempor. Pellentesque et lacinia neque, at interdum orci. Aliquam
+        erat volutpat. Vivamus neque tellus, auctor sed accumsan non, porttitor
+        ac ex. Fusce placerat pellentesque elit, a interdum lacus tristique at.
+        Nulla vitae bibendum eros. Phasellus ut imperdiet nibh.
+      </p>
+      <h3>III. Section 3</h3>
+      <p>
+        Praesent convallis, lorem at mattis varius, tortor nisi facilisis urna,
+        eget dignissim erat lectus pretium lacus. Integer non dignissim odio.
+        Maecenas vitae tincidunt diam. Donec sem augue, eleifend at metus eu,
+        mattis consectetur ex. Quisque eget nisl eget ex lobortis tincidunt
+        vitae vel mauris. Nunc tellus ex, vehicula rutrum diam sed, varius
+        finibus ex. Mauris faucibus leo velit, id sodales nulla malesuada a.
+        Etiam aliquam, tellus et suscipit aliquam, eros orci semper sapien, sit
+        amet semper dui ligula nec ex. Aliquam consequat varius lobortis.
+        Pellentesque vulputate orci non erat mattis scelerisque. Donec tincidunt
+        iaculis mauris id viverra. Aenean ligula nisl, lacinia vitae rutrum
+        eget, congue cursus ipsum. Duis eget mattis purus. In dapibus diam
+        sapien, et dictum lacus pharetra at.
+      </p>
+    </div>
+  </div>
 </template>
 
-<style scoped lang="scss"></style>
+<style scoped lang="scss">
+.main {
+  background-color: white;
+  border-radius: 10px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  padding: 60px;
+}
+
+.main h2 {
+  font-weight: bold;
+}
+
+.main h3 {
+  font-weight: bold;
+}
+</style>
