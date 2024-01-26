3d9cd2b06102a2d42205a409218c28ea14bab8aa
angelaronquillo <142771539+angelaronquillo@users.noreply.github.com>
2024-01-27 01:42:45 +0800

    Customerr order page
    forms
    labels

--- pages/customerorderpage.vue
+++ pages/customerorderpage.vue
@@ -1,24 +1,107 @@
 <template>
   <div class="main">
     <div class="header">
-      <h4>Customize your order</h4>
+      <div class="edit-icon">
+        <img src="/icons/edit.svg" alt="Customize Order" />
+      </div>
+      <div class="title">
+        <h3>Customize your order</h3>
+      </div>
     </div>
+    <hr />
     <div class="content">
       <form>
-        <label for="fname">1. Select size of the cake</label><br />
-        <select id="cars" name="cars">
-          <option value="volvo">1/2 slice (1-2 pax)</option>
-          <option value="saab">1/2 slice (1-2 pax)</option>
-          <option value="fiat">1/2 slice (1-2 pax)</option>
-          <option value="audi">1/2 slice (1-2 pax)</option>
+        <label for="size"> <h4>1. Select size of the cake</h4> </label><br />
+        <select id="size" name="size" class="form-select">
+          <option value="">Select size</option>
+          <option value="half">1/2 slice (1-2 pax)</option>
+          <option value="whole">1 whole (5-6 pax)</option>
+          <option value="fourth">1/4 slice (1 pax)</option>
+          <option value="two">2 slices (3-4 pax)</option>
         </select>
         <br />
-        <label for="lname">2. Tell us about the details</label><br />
-        <input type="text" id="lname" name="lname" /><br />
-        <label for="lname">2. Delivery details</label><br />
-        <input type="text" id="name" name="name" /><br />
-        <label for="lname">2. Delivery Date and Time</label><br />
-        <input type="text" id="ame" name="ame" /><br />
+        <label for="details"><h4>2. Tell us about the details</h4></label><br />
+        <input
+          type="text"
+          id="details"
+          name="details"
+          class="form-control"
+          placeholder="Flavor, Message on cake, Additional detailing etc."
+        /><br />
+        <label for="delivery"><h4>3. Delivery details</h4></label><br />
+        <input
+          type="name"
+          id="name"
+          name="name"
+          placeholder="Name"
+          class="form-control"
+        /><br />
+        <input
+          type="tel"
+          id="num"
+          name="num"
+          class="form-control"
+          placeholder="Enter your phone no."
+        /><br />
+        <!-- Metro Manila-->
+        <select id="city" name="city" class="form-select">
+          <option value="">Select City</option>
+          <option value="Caloocan">Caloocan</option>
+          <option value="Calumpang">Calumpang</option>
+          <option value="Dasmariñas Village">Dasmariñas Village</option>
+          <option value="Ermita">Ermita</option>
+          <option value="Intramuros">Intramuros</option>
+          <option value="Las Piñas">Las Piñas</option>
+          <option value="Makati">Makati</option>
+          <option value="Malabon">Malabon</option>
+          <option value="Malate">Malate</option>
+          <option value="Mandaluyong">Mandaluyong</option>
+          <option value="Manila">Manila</option>
+          <option value="Marikina">Marikina</option>
+          <option value="Muntinlupa">Muntinlupa</option>
+          <option value="National Capital Region">
+            National Capital Region
+          </option>
+          <option value="Navotas">Navotas</option>
+          <option value="Niugan">Niugan</option>
+          <option value="Paco">Paco</option>
+          <option value="Pandacan">Pandacan</option>
+          <option value="Parañaque">Parañaque</option>
+          <option value="Pasay">Pasay</option>
+          <option value="Pasig">Pasig</option>
+          <option value="Pateros">Pateros</option>
+          <option value="Port Area">Port Area</option>
+          <option value="Quezon City">Quezon City</option>
+          <option value="Quiapo">Quiapo</option>
+          <option value="Sambayanihan People's Village">
+            Sambayanihan People's Village
+          </option>
+          <option value="San Juan">San Juan</option>
+          <option value="San Miguel">San Miguel</option>
+          <option value="Santa Ana">Santa Ana</option>
+          <option value="Santa Cruz">Santa Cruz</option>
+          <option value="Singkamas">Singkamas</option>
+          <option value="Taguig">Taguig</option>
+          <option value="Tanza">Tanza</option>
+          <option value="Tanza">Tanza</option>
+          <option value="Tondo">Tondo</option>
+          <option value="Valenzuela">Valenzuela</option></select
+        ><br />
+        <label for="date"><h4>4. Delivery Date and Time</h4></label><br />
+        <input
+          type="date"
+          id="date"
+          name="date"
+          class="form-control"
+          placeholder="Delivery Date"
+        /><br />
+        <input
+          type="time"
+          id="time"
+          name="time"
+          class="form-control"
+          placeholder="Delivery Time"
+        /><br />
       </form>
     </div>
   </div>
@@ -28,21 +111,45 @@
 .main {
   background-color: #77a042;
   border-radius: 15px;
-  margin-top: 40px;
-  margin-bottom: 40px;
+  margin-top: 50px;
+  margin-bottom: 50px;
   padding: 60px;
 }
 
-.main h4 {
+.main h3 {
   color: white;
   font-weight: bold;
+}
+
+.main hr {
+  background-color: white;
+  position: relative;
+  top: 20px;
+  border: none;
+  height: 5px;
+  margin-bottom: 50px;
 }
 
 .content {
   text-align: center !important;
   background-color: #d9d9d9;
   border-radius: 15px;
-  padding: 30px;
   margin-top: 40px;
+  width: 45%;
+  margin: auto;
+  padding: 40px;
+}
+
+.content h4 {
+  font-weight: bold;
+  margin-bottom: 15px;
+}
+
+.header {
+  display: flex;
+}
+
+.edit-icon {
+  margin-right: 15px;
 }
 </style>
