66c7b02a63c50b6990230c11e4d734fa30d5ecf1
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-01-26 15:47:35 +0800

    Semi Complete Review Page
    Complete almost everything for the review page except for the star rating. Used "star" as a place holder.

--- pages/review.vue
+++ pages/review.vue
@@ -11,29 +11,82 @@ definePageMeta({
 </script>
 
 <template>
-  <h1>Review</h1>
   <div class="card">
-    <img src="public\images\reviewPlacehold.png" class="image" />
+    <img src="\images\reviewPlacehold.png" />
     <h2>Slime</h2>
+    <div style="text-align: center">Star</div>
+    <div class="review">
+      <h6>Tell us more!</h6>
+      <textarea placeholder="How did you like your food?"></textarea>
+      <div class="flex-parent jc-center">
+        <button class="button" style="background-color: black; color: white">
+          Submit
+        </button>
+        <button class="button" style="background-color: white; color: black">
+          Back
+        </button>
+      </div>
+    </div>
   </div>
 </template>
 
 <style scoped lang="scss">
 .card {
-  background-color: #3a5b3e;
+  background-color: #77a042;
   display: block;
   margin-left: auto;
   margin-right: auto;
+  margin-top: 5%;
+  margin-bottom: 4%;
+  display: flex;
+  justify-content: center;
+  width: 50%;
 }
 .card img {
   display: block;
   margin-left: auto;
   margin-right: auto;
   border-radius: 50%;
-  padding-top: 10%;
+  margin-top: 5%;
 }
 .card h2 {
-  padding-top: 5%;
+  padding-top: 2%;
   text-align: center;
+}
+.review {
+  margin-left: auto;
+  margin-right: auto;
+  width: 60%;
+  margin-bottom: 5%;
+}
+.review textarea {
+  margin-bottom: 2%;
+  margin-left: auto;
+  margin-right: auto;
+  padding: 2%;
+  width: 100%;
+}
+.review button {
+  border: none;
+  width: 30%;
+  height: 50px;
+  text-align: center;
+  display: inline-block;
+  margin-top: 2%;
+  margin-left: auto;
+  margin-right: auto;
+  margin-bottom: 2%;
+  cursor: pointer;
+}
+.flex-parent {
+  display: flex;
+}
+
+.jc-center {
+  justify-content: center;
+}
+
+button.margin-right {
+  margin-right: 20px;
 }
 </style>
