8d8eb5ce31df3249e1f93654d629ad7d25a623c1
RodPoli <142408199+RodPoli@users.noreply.github.com>
2024-01-30 13:11:22 +0800

    Update review.vue

--- pages/review.vue
+++ pages/review.vue
@@ -14,7 +14,18 @@ definePageMeta({
   <div class="card">
     <img src="\images\reviewPlacehold.png" />
     <h2>Slime</h2>
-    <div style="text-align: center">Star</div>
+    <div class="rate">
+      <input type="radio" id="star5" name="rate" value="5" />
+      <label for="star5" title="text">5 stars</label>
+      <input type="radio" id="star4" name="rate" value="4" />
+      <label for="star4" title="text">4 stars</label>
+      <input type="radio" id="star3" name="rate" value="3" />
+      <label for="star3" title="text">3 stars</label>
+      <input type="radio" id="star2" name="rate" value="2" />
+      <label for="star2" title="text">2 stars</label>
+      <input type="radio" id="star1" name="rate" value="1" />
+      <label for="star1" title="text">1 star</label>
+    </div>
     <div class="review">
       <h6>Tell us more!</h6>
       <textarea placeholder="How did you like your food?"></textarea>
@@ -88,5 +99,46 @@ definePageMeta({
 
 button.margin-right {
   margin-right: 20px;
+}
+* {
+  margin: 0;
+  padding: 0;
+}
+.rate {
+  float: left;
+  height: 46px;
+  padding: 0 10px;
+  margin-left: auto;
+  margin-right: auto;
+}
+.rate:not(:checked) > input {
+  position: absolute;
+  top: -9999px;
+}
+.rate:not(:checked) > label {
+  float: right;
+  width: 1em;
+  overflow: hidden;
+  white-space: nowrap;
+  cursor: pointer;
+  font-size: 30px;
+  color: #ccc;
+}
+.rate:not(:checked) > label:before {
+  content: "★ ";
+}
+.rate > input:checked ~ label {
+  color: #ffc700;
+}
+.rate:not(:checked) > label:hover,
+.rate:not(:checked) > label:hover ~ label {
+  color: #deb217;
+}
+.rate > input:checked + label:hover,
+.rate > input:checked + label:hover ~ label,
+.rate > input:checked ~ label:hover,
+.rate > input:checked ~ label:hover ~ label,
+.rate > label:hover ~ input:checked ~ label {
+  color: #c59b08;
 }
 </style>
