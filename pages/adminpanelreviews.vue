36f50d2ab61f8b7156cc6aa2a5ae4ca9ce9cf0c6
Emman De Guzman <142414248+Emman-DG@users.noreply.github.com>
2024-02-03 23:33:24 +0800

    Done - Admin Panel Reviews
    Finished product of admin panel reviews

---
+++ pages/adminpanelreviews.vue
@@ -0,0 +1,58 @@
+<script setup lang="ts">
+definePageMeta({
+  title: "Menu",
+  meta: [
+    {
+      name: "description",
+      content: `Llana's Cheesecake offers a wide variety of pastry`,
+    }, // TODO: SEO Management
+  ],
+});
+</script>
+
+<template>
+  <div class="review-container">
+    <h2>Reviews</h2>
+
+    <div class="cards">
+      <div class="card">
+        <div class="card-top">
+          <div class="name">
+            <div class="img one" alt="">VT</div>
+            <p>Vincent Tampos</p>
+            <ul class="email">
+              vincenttampos@gmail.com
+            </ul>
+          </div>
+        </div>
+
+        <div class="card-content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            consequuntur, impedit ullam provident deserunt repellendus fuga
+            totam exercitationem minima molestias?
+          </p>
+        </div>
+      </div>
+      <div class="card">
+        <div class="card-top">
+          <div class="name">
+            <div class="img one" alt="">VT</div>
+            <p>Vincent Tampos</p>
+            <ul class="email">
+              vincenttampos@gmail.com
+            </ul>
+          </div>
+        </div>
+
+        <div class="card-content">
+          <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Quidem
+            consequuntur, impedit ullam provident deserunt repellendus fuga
+            totam exercitationem minima molestias?
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
