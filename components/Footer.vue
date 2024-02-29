f303bcccf8cbb53b6c8f17d20762d2ece14f4872
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-02-29 15:25:43 +0800

    Fix social media links

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,15 @@
 <template>
   <footer class="d-flex align-items-center flex-column container-fluid py-3">
     <div class="d-flex justify-content-evenly">
-      <a href="https://www.facebook.com/dairyblend2022"><img class="w-25 my-3 mx-5" src="/images/fb_icon_v2_s.png" /></a>
-      <a href="https://www.facebook.com/dairyblend2022"><img class="w-25 my-3 mx-5" src="/images/twitter_icon_v2_s.png" /></a>
-      <a href="https://www.facebook.com/dairyblend2022"><img class="w-25 my-3 mx-5" src="/images/ig_icon_v2_s.png" /></a>
+      <a href=" https://web.facebook.com/llanaa.ph" target="_blank">
+        <img class="w-25 my-3 mx-5" src="/images/fb_icon_v2_s.png" />
+      </a>
+      <a href="#">
+        <img class="w-25 my-3 mx-5" src="/images/twitter_icon_v2_s.png" />
+      </a>
+      <a href="https://www.instagram.com/llanaa.ph" target="_blank">
+        <img class="w-25 my-3 mx-5" src="/images/ig_icon_v2_s.png" />
+      </a>
     </div>
 
     <div class="d-flex justify-content-evenly my-3">
