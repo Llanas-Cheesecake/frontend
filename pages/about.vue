11f30432e735177f1b0d5dfaf676805314a50eba
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-03-04 00:01:40 +0800

    Rename 'aboutus.vue' to 'about.vue'

--- pages/about.vue
+++ pages/about.vue
@@ -1,11 +1,69 @@
-<script setup lang="ts">
-
-</script>
-
 <template>
-  <div>About</div>
+  <div class="main">
+    <div class="header">
+      <h2>About Llana's Cheesecake</h2>
+    </div>
+    <div class="content">
+      <img
+        src="/images/llana_logo_m.png"
+        alt="Llana's Cheesecake Logo"
+        class="rounded mx-auto d-block mb-4"
+      />
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
+        feugiat vel est a iaculis. Nullam sit amet mi id felis iaculis feugiat.
+        Sed a condimentum orci. Duis sodales libero eget purus sollicitudin
+        condimentum at ullamcorper sem. Donec risus elit, luctus ut cursus eget,
+        rhoncus vel est. Donec sollicitudin justo augue, id dapibus justo
+        imperdiet at. Phasellus et enim sit amet quam facilisis aliquam ac a
+        urna. Aenean tristique urna dui, a semper sem lobortis eu. Aenean auctor
+        euismod velit, vel sollicitudin risus egestas et. Integer sed leo
+        maximus, laoreet magna ac, volutpat sem. Aenean nec sagittis tortor. In
+        facilisis ipsum et imperdiet rhoncus. Nullam lobortis, purus pulvinar
+        elementum aliquet, felis diam venenatis leo, vitae malesuada urna nulla
+        vel risus. Nunc gravida justo lectus.
+      </p>
+      <p>
+        Morbi non tellus et mi condimentum malesuada. Pellentesque lacus ante,
+        volutpat eu placerat sed, vulputate in dui. Nullam at ex nec turpis
+        laoreet tempor. Pellentesque et lacinia neque, at interdum orci. Aliquam
+        erat volutpat. Vivamus neque tellus, auctor sed accumsan non, porttitor
+        ac ex. Fusce placerat pellentesque elit, a interdum lacus tristique at.
+        Nulla vitae bibendum eros. Phasellus ut imperdiet nibh.
+      </p>
+      <p>
+        Praesent convallis, lorem at mattis varius, tortor nisi facilisis urna,
+        eget dignissim erat lectus pretium lacus. Integer non dignissim odio.
+        Maecenas vitae tincidunt diam. Donec sem augue, eleifend at metus eu,
+        mattis consectetur ex. Quisque eget nisl eget ex lobortis tincidunt
+        vitae vel mauris. Nunc tellus ex, vehicula rutrum diam sed, varius
+        finibus ex. Mauris faucibus leo velit, id sodales nulla malesuada a.
+        Etiam aliquam, tellus et suscipit aliquam, eros orci semper sapien, sit
+        amet semper dui ligula nec ex. Aliquam consequat varius lobortis.
+        Pellentesque vulputate orci non erat mattis scelerisque. Donec tincidunt
+        iaculis mauris id viverra. Aenean ligula nisl, lacinia vitae rutrum
+        eget, congue cursus ipsum. Duis eget mattis purus. In dapibus diam
+        sapien, et dictum lacus pharetra at.
+      </p>
+    </div>
+  </div>
 </template>
 
 <style scoped lang="scss">
+.main {
+  background-color: white;
+  border-radius: 10px;
+  margin-top: 40px;
+  margin-bottom: 40px;
+  padding: 60px;
+}
 
+.main h2 {
+  font-weight: bold;
+  text-align: center;
+}
+
+.header {
+  margin-bottom: 20px;
+}
 </style>
