e3dd38ff2a0088ba076d092db83ac57a4fe69e66
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-03-04 00:57:47 +0800

    Initial work on admin panel - Moved app.vue styling to default layout

---
+++ layouts/admin.vue
@@ -0,0 +1,23 @@
+<script setup lang="ts">
+const route = useRoute();
+
+useHead({
+  titleTemplate: (title) => {
+    return title
+        ? `${title} - Admin Panel - Llana's Cheesecake`
+        : 'Llana\'s Cheesecake'
+  }
+})
+</script>
+
+<template>
+  <div class="admin-panel">
+    <section class="content">
+      <slot />
+    </section>
+  </div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ pages/admin/index.vue
@@ -0,0 +1,13 @@
+<script setup lang="ts">
+  definePageMeta({
+    layout: 'admin'
+  })
+</script>
+
+<template>
+  <div>test</div>
+</template>
+
+<style scoped lang="scss">
+
+</style>
