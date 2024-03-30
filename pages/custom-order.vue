71027bd8008c09cc1a544fedf2ac66deabef6ace
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-03-31 01:18:07 +0800

    Add form validation and missing email field

--- pages/custom-order.vue
+++ pages/custom-order.vue
@@ -11,6 +11,7 @@
 
   const form = reactive({
     customer_name: '',
+    customer_email: '',
     customer_phone_number: '',
     customer_courier: '',
     cake_size: '8 inches',
@@ -20,8 +21,9 @@
 
   const errorMessage = ref('');
 
-  const validation = ref({
+  const validation = reactive({
     customer_name: [],
+    customer_email: [],
     customer_phone_number: [],
     customer_courier: [],
     cake_size: [],
@@ -38,6 +40,7 @@
       method: "POST",
       body: {
         customer_name: form.customer_name,
+        customer_email: form.customer_email,
         customer_phone_number: form.customer_phone_number,
         customer_courier: form.customer_courier,
         cake_size: form.cake_size,
@@ -60,7 +63,13 @@
 
       switch (error.value.statusCode) {
         case 422:
-          validation.value = { ...payload.errors };
+          validation.customer_name = payload.errors.customer_name || [];
+          validation.customer_email = payload.errors.customer_email || [];
+          validation.customer_phone_number = payload.errors.customer_phone_number || [];
+          validation.customer_courier = payload.errors.customer_courier || [];
+          validation.cake_size = payload.errors.cake_size || [];
+          validation.cake_description = payload.errors.cake_description || [];
+          validation.delivery_datetime = payload.errors.delivery_datetime || [];
           break;
         default:
           errorMessage.value = payload.message
@@ -69,20 +78,20 @@
   }
 
   const resetValidations = () => {
-    validation.value = {
-      customer_name: [],
-      customer_phone_number: [],
-      customer_courier: [],
-      cake_size: [],
-      cake_description: [],
-      delivery_datetime: []
-    };
+    validation.customer_name = [];
+    validation.customer_email = [];
+    validation.customer_phone_number = [];
+    validation.customer_courier = [];
+    validation.cake_size = [];
+    validation.cake_description = [];
+    validation.delivery_datetime = [];
 
     errorMessage.value = '';
   }
 
   const resetForm = () => {
     form.customer_name = '';
+    form.customer_email = '';
     form.customer_phone_number = '';
     form.customer_courier = '';
     form.cake_size = '8 inches';
@@ -100,7 +109,12 @@
     <div class="card p-2">
       <div class="card-body">
 
-        <h5 class="fw-bold mb-4">Customize your order</h5>
+        <h5 class="fw-bold mb-2">
+          Customize your order
+        </h5>
+        <p class="mb-5">
+          Please do note that we only do pick-up services. You must provide your own delivery courier.
+        </p>
 
         <div v-if="errorMessage.length > 0" class="alert alert-danger mb-4" role="alert">
           {{ errorMessage }}
@@ -117,43 +131,73 @@
               1. Choose your size
             </h5>
             <div class="form-floating">
-              <select v-model="form.cake_size" class="form-select" :disabled="isSubmitting">
+              <select v-model="form.cake_size" class="form-select" :class="{ 'is-invalid': validation.cake_size.length > 0 }" :disabled="isSubmitting">
                 <option value="8 inches">8" (8 inches)</option>
                 <option value="6 inches">6" (6 inches)</option>
               </select>
               <label>Size</label>
+
+              <small v-for="error in validation.cake_size" class="invalid-feedback">
+                {{ error }}
+              </small>
             </div>
           </div>
 
           <div class="mb-4">
             <h5 class="fw-bold mb-3">2. Tell us about the order</h5>
             <div class="form-floating">
-              <textarea v-model="form.cake_description" class="form-control" :disabled="isSubmitting" type="text" placeholder="" style="height: 100px;"></textarea>
+              <textarea v-model="form.cake_description" class="form-control" :class="{ 'is-invalid': validation.cake_description.length > 0 }" :disabled="isSubmitting" type="text" placeholder="" style="height: 100px;"></textarea>
               <label class="form-label">Describe your cake here</label>
+
+              <small v-for="error in validation.cake_description" class="invalid-feedback">
+                {{ error }}
+              </small>
             </div>
           </div>
 
           <div class="mb-4">
-            <h5 class="fw-bold mb-3">
-              3. Delivery Details
+            <h5 class="fw-bold mb-2">
+              3. Customer Details
             </h5>
+            <p class="mb-4">
+              We will use this to contact you.
+            </p>
             <div class="form-floating mb-3">
-              <input v-model="form.customer_name" class="form-control mb-2" :disabled="isSubmitting" placeholder="John Doe" aria-label="Name">
-              <label class="form-label">Your name</label>
+              <input v-model="form.customer_name" type="text" class="form-control mb-2" :class="{ 'is-invalid': validation.customer_name.length > 0 }" :disabled="isSubmitting" placeholder="John Doe" aria-label="Name">
+              <label class="form-label">Name</label>
+
+              <small v-for="error in validation.customer_name" class="invalid-feedback">
+                {{ error }}
+              </small>
             </div>
             <div class="form-floating mb-3">
+              <input v-model="form.customer_email" type="email" class="form-control mb-2" :class="{ 'is-invalid': validation.customer_email.length > 0 }" :disabled="isSubmitting" placeholder="John Doe" aria-label="Name">
+              <label class="form-label">Email</label>
+
+              <small v-for="error in validation.customer_email" class="invalid-feedback">
+                {{ error }}
+              </small>
+            </div>
+            <div class="mb-3">
               <div class="input-group">
                 <div class="input-group-text">+63</div>
-                <div class="form-floating">
-                  <input v-model="form.customer_phone_number" type="text" class="form-control" :disabled="isSubmitting" placeholder="e.g. 9123456789">
+                <div class="form-floating" :class="{ 'is-invalid': validation.customer_phone_number.length > 0 }">
+                  <input v-model="form.customer_phone_number" type="text" class="form-control" :class="{ 'is-invalid': validation.customer_phone_number.length > 0 }" :disabled="isSubmitting" placeholder="e.g. 9123456789">
                   <label class="form-label">Phone Number</label>
                 </div>
+                <small v-for="error in validation.customer_phone_number" class="invalid-feedback">
+                  {{ error }}
+                </small>
               </div>
             </div>
             <div class="form-floating">
               <div class="form-floating mb-3">
-                <input v-model="form.customer_courier" class="form-control mb-2" :disabled="isSubmitting" placeholder="John Doe" aria-label="Name">
+                <input v-model="form.customer_courier" class="form-control mb-2" :class="{ 'is-invalid': validation.customer_courier.length > 0 }" :disabled="isSubmitting" placeholder="John Doe" aria-label="Name">
                 <label class="form-label">Your chosen courier</label>
+
+                <small v-for="error in validation.customer_courier" class="invalid-feedback">
+                  {{ error }}
+                </small>
               </div>
             </div>
           </div>
@@ -162,8 +206,12 @@
             <h5 class="fw-bold mb-3">4. Delivery Date and Time</h5>
             <div class="col">
               <div class="form-floating">
-                <input v-model="form.delivery_datetime" class="form-control mb-2" :disabled="isSubmitting" type="datetime-local" aria-label="DateTime">
+                <input v-model="form.delivery_datetime" class="form-control mb-2" :class="{ 'is-invalid': validation.delivery_datetime.length > 0 }" :disabled="isSubmitting" type="datetime-local" aria-label="DateTime">
                 <label class="form-label">Choose your preferred date and time</label>
+
+                <small v-for="error in validation.delivery_datetime" class="invalid-feedback">
+                  {{ error }}
+                </small>
               </div>
             </div>
           </div>
