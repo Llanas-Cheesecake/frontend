fa2725c5d97637aed0699aa3bbecc79d8367d976
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-01 23:26:49 +0800

    Updated terms as provided by the client

--- pages/termsofuse.vue
+++ pages/termsofuse.vue
@@ -1,67 +1,127 @@
+<script setup lang="ts">
+
+</script>
+
 <template>
-  <div class="main">
-    <div class="header">
-      <h2>Terms of Use</h2>
-      <p>Last updated December 16, 2023</p>
-    </div>
-    <div class="content">
-      <h3>I. Section 1</h3>
-      <p>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas
-        feugiat vel est a iaculis. Nullam sit amet mi id felis iaculis feugiat.
-        Sed a condimentum orci. Duis sodales libero eget purus sollicitudin
-        condimentum at ullamcorper sem. Donec risus elit, luctus ut cursus eget,
-        rhoncus vel est. Donec sollicitudin justo augue, id dapibus justo
-        imperdiet at. Phasellus et enim sit amet quam facilisis aliquam ac a
-        urna. Aenean tristique urna dui, a semper sem lobortis eu. Aenean auctor
-        euismod velit, vel sollicitudin risus egestas et. Integer sed leo
-        maximus, laoreet magna ac, volutpat sem. Aenean nec sagittis tortor. In
-        facilisis ipsum et imperdiet rhoncus. Nullam lobortis, purus pulvinar
-        elementum aliquet, felis diam venenatis leo, vitae malesuada urna nulla
-        vel risus. Nunc gravida justo lectus.
-      </p>
-      <h3>II. Section 2</h3>
-      <p>
-        Morbi non tellus et mi condimentum malesuada. Pellentesque lacus ante,
-        volutpat eu placerat sed, vulputate in dui. Nullam at ex nec turpis
-        laoreet tempor. Pellentesque et lacinia neque, at interdum orci. Aliquam
-        erat volutpat. Vivamus neque tellus, auctor sed accumsan non, porttitor
-        ac ex. Fusce placerat pellentesque elit, a interdum lacus tristique at.
-        Nulla vitae bibendum eros. Phasellus ut imperdiet nibh.
-      </p>
-      <h3>III. Section 3</h3>
-      <p>
-        Praesent convallis, lorem at mattis varius, tortor nisi facilisis urna,
-        eget dignissim erat lectus pretium lacus. Integer non dignissim odio.
-        Maecenas vitae tincidunt diam. Donec sem augue, eleifend at metus eu,
-        mattis consectetur ex. Quisque eget nisl eget ex lobortis tincidunt
-        vitae vel mauris. Nunc tellus ex, vehicula rutrum diam sed, varius
-        finibus ex. Mauris faucibus leo velit, id sodales nulla malesuada a.
-        Etiam aliquam, tellus et suscipit aliquam, eros orci semper sapien, sit
-        amet semper dui ligula nec ex. Aliquam consequat varius lobortis.
-        Pellentesque vulputate orci non erat mattis scelerisque. Donec tincidunt
-        iaculis mauris id viverra. Aenean ligula nisl, lacinia vitae rutrum
-        eget, congue cursus ipsum. Duis eget mattis purus. In dapibus diam
-        sapien, et dictum lacus pharetra at.
-      </p>
+  <div class="card px-4 py-3 my-5">
+    <div class="card-body">
+      <div class="header">
+        <h2>Terms and Conditions</h2>
+        <p>Last updated April 1, 2024</p>
+      </div>
+      <div class="mb-4">
+        <p>
+          Welcome to Llana's Cheesecake! These terms and conditions outline the rules and regulations for the use of our website.
+        </p>
+
+        <p>
+          By accessing this website, we assume you accept these terms and conditions. Do not continue to use Llana's Cheesecake if you do not agree to all of the terms and conditions stated on this page.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Definitions</h3>
+
+        <p>
+          The following terminology applies to these Terms and Conditions:
+        </p>
+
+        <ul>
+          <li>"Customer," "You," and "Your" refers to you, the person accessing this website and accepting the Company's terms and conditions.</li>
+          <li>"The Company," "Ourselves," "We," "Our," and "Us," refers to Llana's Cheesecake.</li>
+          <li>"Party," "Parties," or "Us," refers to both the Customer and ourselves.</li>
+          <li>"Website" refers to Llana's Cheesecake's website.</li>
+        </ul>
+      </div>
+
+      <div class="mb-4">
+        <h3>Cookies</h3>
+        <p>
+          We employ the use of cookies. By accessing Llana's Cheesecake, you agreed to use cookies in agreement with Llana's Cheesecake's Privacy Policy.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>License</h3>
+        <p>
+          Unless otherwise stated, Llana's Cheesecake and/or its licensors own the intellectual property rights for all material on Llana's Cheesecake. All intellectual property rights are reserved. You may access this from Llana's Cheesecake for your own personal use subjected to restrictions set in these terms and conditions.
+        </p>
+
+        <h5 class="fw-bold">You must not:</h5>
+
+        <ul>
+          <li>Republish material from Llana's Cheesecake</li>
+          <li>Sell, rent, or sub-license material from Llana's Cheesecake</li>
+          <li>Reproduce, duplicate, or copy material from Llana's Cheesecake</li>
+          <li>Redistribute content from Llana's Cheesecake</li>
+        </ul>
+      </div>
+
+      <div class="mb-4">
+        <h3>Refunds</h3>
+        <p>
+          Once a payment has been made for our products or services, refunds are not allowed. Any damages or delay after the product has been dispatched will not held us liable. We take great care in providing our customers with quality products and services.
+        </p>
+        <p>
+          If you have any concerns about your purchase, please contact us at  8470-6075/<a href="mailto:llanaa.ph@gmail.com">llanaa.ph@gmail.com</a> and we will strive to address your concerns.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Reservations of Rights</h3>
+        <p>
+          We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Content Liability</h3>
+        <p>
+          We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that arise on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene, or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of any third party rights.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Warranty</h3>
+        <p>
+          We make no warranties, representations, statements, or guarantees (whether express, implied in law, or residual) regarding Llana's Cheesecake and the website, its services, or the information contained on the website.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Indemnification</h3>
+        <p>
+          You hereby indemnify to the fullest extent Llana's Cheesecake from and against any and/or all liabilities, costs, demands, causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of these Terms.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Severability</h3>
+        <p>
+          If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <h3>Changes to Terms</h3>
+        <p>
+          We reserve the right to revise these terms and conditions at any time as we see fit. By using this Website, you are expected to review these terms on a regular basis.
+        </p>
+      </div>
+
+      <div class="mb-4">
+        <p>
+          If you have any questions about these Terms and Conditions, please contact us at 8470-6075 / <a href="mailto:llanaa.ph@gmail.com">llanaa.ph@gmail.com</a>
+        </p>
+        <hr class="my-5" />
+        <p class="mb-0">
+          By using our website, you hereby consent to our terms and conditions and agree to be bound by them.
+        </p>
+      </div>
     </div>
   </div>
 </template>
 
 <style scoped lang="scss">
-.main {
-  background-color: white;
-  border-radius: 10px;
-  margin-top: 40px;
-  margin-bottom: 40px;
-  padding: 60px;
-}
 
-.main h2 {
-  font-weight: bold;
-}
-
-.main h3 {
-  font-weight: bold;
-}
 </style>
