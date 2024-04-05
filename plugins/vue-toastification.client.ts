3a25b62696491e0b7a3038574fd0adb9b937a603
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-05 21:12:34 +0800

    Show toast messages in the center

--- plugins/vue-toastification.client.ts
+++ plugins/vue-toastification.client.ts
@@ -2,7 +2,7 @@ import Toast, {type PluginOptions, POSITION} from "vue-toastification";
 
 export default defineNuxtPlugin((nuxt) => {
     const options: PluginOptions = {
-        position: POSITION.BOTTOM_RIGHT
+        position: POSITION.BOTTOM_CENTER
     }
 
     nuxt.vueApp.use(Toast, options)
