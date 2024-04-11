ceb79664bd0954a7b719e32272da6c887d279d0c
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-11 13:38:50 +0800

    Refactor "Alphabet" to "Alphabet: A-Z"

--- pages/menu/[slug].vue
+++ pages/menu/[slug].vue
@@ -47,7 +47,7 @@
     <!-- Top Buttons -->
     <div class="col-sm-12 col-md-4">
       <select class="form-select" aria-label="Sort By" v-model="sortedBy">
-        <option value="default">Alphabet</option>
+        <option value="default">Alphabet: A-Z</option>
         <option value="best-seller">Best sellers</option>
         <option value="price-max-to-min">Price: High to Low</option>
         <option value="price-min-to-max">Price: Low to High</option>
