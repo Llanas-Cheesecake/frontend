4ec482bd8619cc4963431a0daa3b59e0426cd5db
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-21 17:44:53 +0800

    Footer: Add opening hours & renamed "Contact Info" to "Store Info"

--- components/Footer.vue
+++ components/Footer.vue
@@ -60,23 +60,29 @@
           </ul>
         </div>
         <div class="col-sm-12 col-md-4">
-          <h2 class="text-heading fw-bold">Contact Info</h2>
+          <h2 class="text-heading fw-bold">Store Info</h2>
 
           <ul class="list-unstyled">
             <li class="mb-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="top: -1px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-map-pin position-relative">
                 <path d="M21 10c0 7-9 13-9 13s-9-6-9-13a9 9 0 0 1 18 0z"></path><circle cx="12" cy="10" r="3"></circle>
               </svg>
               <span class="ms-3">54 Scout Gandia Brgy. Laging Handa QC</span>
             </li>
             <li class="mb-2">
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="top: -1px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-clock position-relative">
+                <circle cx="12" cy="12" r="10"></circle><polyline points="12 6 12 12 16 14"></polyline>
+              </svg>
+              <span class="ms-3">Open daily at 12:00 pm- 10:00 pm</span>
+            </li>
+            <li class="mb-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="top: -1px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-phone position-relative">
                 <path d="M22 16.92v3a2 2 0 0 1-2.18 2 19.79 19.79 0 0 1-8.63-3.07 19.5 19.5 0 0 1-6-6 19.79 19.79 0 0 1-3.07-8.67A2 2 0 0 1 4.11 2h3a2 2 0 0 1 2 1.72 12.84 12.84 0 0 0 .7 2.81 2 2 0 0 1-.45 2.11L8.09 9.91a16 16 0 0 0 6 6l1.27-1.27a2 2 0 0 1 2.11-.45 12.84 12.84 0 0 0 2.81.7A2 2 0 0 1 22 16.92z"></path>
               </svg>
               <span class="ms-3">8470-6075</span>
             </li>
-            <li>
-              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail">
+            <li class="mb-2">
+              <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" style="top: -1px;" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round" class="feather feather-mail position-relative">
                 <path d="M4 4h16c1.1 0 2 .9 2 2v12c0 1.1-.9 2-2 2H4c-1.1 0-2-.9-2-2V6c0-1.1.9-2 2-2z"></path><polyline points="22,6 12,13 2,6"></polyline>
               </svg>
               <span class="ms-3">llanaa.ph@gmail.com</span>
