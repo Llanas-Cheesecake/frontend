102728a97d1784f0e934a762dfe806b29f9aedcd
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-04-25 23:20:42 +0800

    Terms and Conditions updated

--- pages/termsandconditions.vue
+++ pages/termsandconditions.vue
@@ -19,7 +19,7 @@
         </p>
       </div>
 
-      <div class="mb-4">
+      <div class="mb-5">
         <h3>Definitions</h3>
 
         <p>
@@ -34,83 +34,213 @@
         </ul>
       </div>
 
-      <div class="mb-4">
-        <h3>License</h3>
+      <section class="mb-5">
+        <h3 class="mb-4">Copyright and License</h3>
+
+        <div class="mb-4">
+          <h4 class="fw-bold">License</h4>
+          <p>
+            Unless otherwise stated, Llana's Cheesecake and its licensors own the intellectual property rights for all material on Llana's Cheesecake.
+          </p>
+
+          <p>
+            All content, including but not limited to text, images, graphics, logos, videos, and recipes, displayed on this website is the intellectual property of Llana's Cheesecake unless otherwise stated.
+          </p>
+
+          <p>
+            Customers of this website may view, download, and print content for personal, non-commercial use only. Any other use, including but not limited to reproduction, modification, distribution, or re-publication, without our written consent is strictly prohibited.
+          </p>
+
+          <p>
+            You may access the website for Llana's Cheesecake for your personal use, subject to restrictions set in these terms and conditions.
+          </p>
+
+          <h5 class="fw-bold">You must not:</h5>
+
+          <ul>
+            <li>Republish material from Llana's Cheesecake</li>
+            <li>Sell, rent, or sub-license material from Llana's Cheesecake</li>
+            <li>Reproduce, duplicate, or copy material from Llana's Cheesecake</li>
+            <li>Redistribute content from Llana's Cheesecake</li>
+          </ul>
+        </div>
+
+        <div>
+          <h4 class="fw-bold">Content Liability</h4>
+
+          <p>
+            No link(s) should appear on any Website that may be interpreted as libelous, obscene, or criminal or which infringes, otherwise violates, or advocates the infringement or other violation of any third-party rights.
+          </p>
+        </div>
+      </section>
+
+      <section class="mb-5">
+        <h3 class="mb-4">Shipping policy</h3>
+
         <p>
-          Unless otherwise stated, Llana's Cheesecake and/or its licensors own the intellectual property rights for all material on Llana's Cheesecake. All intellectual property rights are reserved. You may access this from Llana's Cheesecake for your own personal use subjected to restrictions set in these terms and conditions.
+          You are responsible for declaring and arranging your courier service (such as Grab, Lalamove, etc.).
+        </p>
+        <p>
+          Llana's Cheesecakes will not assume liability for the goods during shipping or transport.
+        </p>
+        <p>
+          We advise reserving four-wheeled vehicles or cars to transport our cheesecakes, as motorcycle rides can be rough and potentially damage the cake. If you still prefer motorcycle couriers, please ensure they use an insulated box for additional security.
+        </p>
+      </section>
+
+      <section class="mb-5">
+        <h3 class="mb-4">Refund and Return Policy</h3>
+
+        <div class="mb-4">
+          <h4 class="fw-bold">Refunds</h4>
+
+          <p>
+            Refunds are not allowed once a payment has been made for our products and services.
+          </p>
+
+          <p>
+            Any damages or delay after the product has been dispatched will not be held against us.
+          </p>
+
+          <p>
+            If you have any concerns about your purchase, please contact us at 8470-6075/llanaa.ph@gmail.com, and we will strive to address them.
+          </p>
+        </div>
+
+        <div class="mb-4">
+          <h4 class="fw-bold">Returns</h4>
+
+          <p>
+            We are not liable for any damages or losses from improperly handling or storing perishable items (e.g., Cheesecakes) after delivery. It is the customer's responsibility to follow instructions for proper storage and consumption.
+          </p>
+
+          <p>
+            Returns will not be accepted. However, if there are any issues with the quality of the products received, please contact us to discuss possible solutions.
+          </p>
+        </div>
+
+        <div>
+          <h4 class="fw-bold">Cancellation of Orders</h4>
+
+          <p>
+            We have the right to cancel orders, including but not limited to the following reasons:
+          </p>
+
+          <ul>
+            <li>Prior communication with the us</li>
+            <li>Upon breach of Terms/Conditions of Service</li>
+          </ul>
+
+          <p>
+            You now agree to the following cancellation procedures:
+          </p>
+
+          <ul>
+            <li>We will contact you as soon as possible</li>
+            <li>We will ask for substitutes that are close to the cancelled product.</li>
+            <li>We will fully refund the order afterwhich, you may choose to order again or cancel it.</li>
+            <li>Any further inquiries regarding the cancellation will be communicated via the live text support chat or through llanaa.ph@gmail.com</li>
+          </ul>
+        </div>
+
+      </section>
+
+      <section class="mb-5">
+        <h3 class="mb-4">Payment and Discount Policy</h3>
+
+        <div class="mb-4">
+          <h4 class="fw-bold">
+            Payment Policy
+          </h4>
+
+          <p>
+            You now adhere to the terms and conditions/policies provided by Paymongo upon payment.
+          </p>
+
+          <p>
+            With this agreement, you agree that according to the Refund and Return Policies, once a payment has been made, you are no longer eligible for refunds. For any inquiries, please contact us using the contact information provided below.
+          </p>
+        </div>
+
+        <div class="">
+          <h4 class="fw-bold">
+            Persons with Disability (PWD) and Senior Citizen Discounts
+          </h4>
+
+          <p>
+            Eligible individuals under RA No. 9994 (Expanded Senior Citizens Act of 2010) and RA No. 10754 (Act Expanding the Benefits and Privileges of PWDs) are entitled to discounts stated by the law.
+          </p>
+
+          <p>
+            Individuals must present a valid and current identification card issued for Persons With Disabilities (PWD) or Senior Citizens.
+          </p>
+
+          <p>
+            Qualified PWDs and Senior Citizens are entitled to a 20% discount on the total cost of goods and services consumable by the individual, as mandated by the law.
+          </p>
+
+          <p>
+            The discount may not apply to items or services already on sale, promotional offers, or items exempted by law. Unless expressly stated, the discount cannot be combined with other discounts or privileges.
+          </p>
+
+          <p>
+            The discount is non-transferable and may only be availed by the qualified PWD or Senior Citizen cardholder. Misuse or unauthorized use of the discount may result in legal action.
+          </p>
+
+          <p>
+            To avail the PWD discount, users are required to create an account on the website and submit the necessary documents on the settings page.
+          </p>
+        </div>
+      </section>
+
+      <section class="mb-5">
+        <h3 class="mb-4">Changes to Terms and Conditions</h3>
+
+        <p>
+          We reserve the right to revise these terms and conditions as we see fit. By using this Website, you are expected to review these terms regularly.
         </p>
 
-        <h5 class="fw-bold">You must not:</h5>
+        <p>
+          Terms and Conditions apply immediately throughout the website after changes have been made.
+        </p>
+      </section>
+
+      <section class="mb-5">
+        <h3 class="mb-4">Contact Information</h3>
+
+        <p>
+          The following are the only recognized ways to communicate with us officially:
+        </p>
 
         <ul>
-          <li>Republish material from Llana's Cheesecake</li>
-          <li>Sell, rent, or sub-license material from Llana's Cheesecake</li>
-          <li>Reproduce, duplicate, or copy material from Llana's Cheesecake</li>
-          <li>Redistribute content from Llana's Cheesecake</li>
+          <li>Telephone Number: 8470-6075</li>
+          <li>Email: llanaa.ph@gmail.com</li>
         </ul>
-      </div>
+      </section>
 
-      <div class="mb-4">
-        <h3>Refunds</h3>
-        <p>
-          Once a payment has been made for our products or services, refunds are not allowed. Any damages or delay after the product has been dispatched will not held us liable. We take great care in providing our customers with quality products and services.
-        </p>
-        <p>
-          If you have any concerns about your purchase, please contact us at  8470-6075/<a href="mailto:llanaa.ph@gmail.com">llanaa.ph@gmail.com</a> and we will strive to address your concerns.
-        </p>
-      </div>
+      <section class="mb-5">
+        <h3 class="mb-4">References</h3>
 
-      <div class="mb-4">
-        <h3>Reservations of Rights</h3>
-        <p>
-          We reserve the right to request that you remove all links or any particular link to our Website. You approve to immediately remove all links to our Website upon request. We also reserve the right to amend these terms and conditions and its linking policy at any time. By continuously linking to our Website, you agree to be bound to and follow these linking terms and conditions.
-        </p>
-      </div>
+        <cite class="d-block mb-2">
+          Republic of the Philippines. (2010). Expanded Senior Citizens Act of 2010, Republic Act No. 9994. https://lawphil.net/statutes/repacts/ra2010/ra_9994_2010.html
+        </cite>
+        <cite class="d-block mb-2">
+          Republic of the Philippines. (2012). Data Privacy Act of 2012, Republic Act No. 10173. https://www.privacy.gov.ph/data-privacy-act-of-2012-republic-act-no-10173/
+        </cite>
+        <cite class="d-block mb-2">
+          Republic of the Philippines. (2015). Magna Carta for Persons with Disability, Republic Act No. 10754. https://www.officialgazette.gov.ph/2016/03/23/republic-act-no-10754/
+        </cite>
+      </section>
 
-      <div class="mb-4">
-        <h3>Content Liability</h3>
+      <section>
         <p>
-          We shall not be held responsible for any content that appears on your Website. You agree to protect and defend us against all claims that arise on your Website. No link(s) should appear on any Website that may be interpreted as libelous, obscene, or criminal, or which infringes, otherwise violates, or advocates the infringement or other violation of any third party rights.
+          If you have any questions about our Privacy Policy, please contact us at 8470-6075 / <a href="mailto:llanaa.ph@gmail.com">llanaa.ph@gmail.com</a>
         </p>
-      </div>
-
-      <div class="mb-4">
-        <h3>Warranty</h3>
-        <p>
-          We make no warranties, representations, statements, or guarantees (whether express, implied in law, or residual) regarding Llana's Cheesecake and the website, its services, or the information contained on the website.
-        </p>
-      </div>
-
-      <div class="mb-4">
-        <h3>Indemnification</h3>
-        <p>
-          You hereby indemnify to the fullest extent Llana's Cheesecake from and against any and/or all liabilities, costs, demands, causes of action, damages, and expenses arising in any way related to your breach of any of the provisions of these Terms.
-        </p>
-      </div>
-
-      <div class="mb-4">
-        <h3>Severability</h3>
-        <p>
-          If any provision of these Terms is found to be invalid under any applicable law, such provisions shall be deleted without affecting the remaining provisions herein.
-        </p>
-      </div>
-
-      <div class="mb-4">
-        <h3>Changes to Terms</h3>
-        <p>
-          We reserve the right to revise these terms and conditions at any time as we see fit. By using this Website, you are expected to review these terms on a regular basis.
-        </p>
-      </div>
-
-      <div class="mb-4">
-        <p>
-          If you have any questions about these Terms and Conditions, please contact us at 8470-6075 / <a href="mailto:llanaa.ph@gmail.com">llanaa.ph@gmail.com</a>
-        </p>
-        <hr class="my-5" />
+        <hr class="my-4" />
         <p class="mb-0">
-          By using our website, you hereby consent to our terms and conditions and agree to be bound by them.
+          By using our website, you hereby consent to our privacy policy and agree to be bound by them.
         </p>
-      </div>
+      </section>
     </div>
   </div>
 </template>
