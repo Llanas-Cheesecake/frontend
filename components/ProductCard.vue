8c37e7e2aa1b13e9d59592a89de1fdd984deb535
Vincent Tampos <56838797+glitchcodes@users.noreply.github.com>
2024-05-15 23:55:57 +0800

    Adjust min-height for product cards for consistency

--- components/ProductCard.vue
+++ components/ProductCard.vue
@@ -75,7 +75,7 @@
 <style scoped lang="scss">
   .card {
     transition: 0.3s;
-    min-height: 385px;
+    min-height: 552px;
     &:hover {
       box-shadow: 0 3px 6px rgba(0,0,0,0.16), 0 3px 6px rgba(0,0,0,0.23);
     }
